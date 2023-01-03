94e8b7f38fb1461eb29484e4cbf784078531dee0
Asanser30 <vasilislionis361@gmail.com>
2023-01-03 19:55:35 +0200

    kostas

--- Results.js
+++ Results.js
@@ -23,27 +23,10 @@ percent.textContent = parseFloat((Correct/apotelesma)*100).toFixed(2) + '%'
 score.textContent = (Correct)*100 
 
 function Restart(){
-<<<<<<< HEAD
     return location.replace('Quiz.html')
 }
 
 function Menu(){
     return location.replace('Menu.html')
 }
-=======
-    restart = 1
-    return location.assign('Quiz.html')
-}
 
-function Menu(){
-    restart = 0
-    return location.assign('Menu.html')
-}
-
-form.addEventListener('submit', function(e){
-    e.preventDefault();
-
-    localStorage.setItem('restart', restart)
-    localStorage.setItem('Tries', tries)
-})
->>>>>>> 87f70da8aac6397c6b2b99b677e6db1cb35837f6
